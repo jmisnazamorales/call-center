958fe2580941d18dd7a65a0408b8287510e4a37f
jmisnaza <julianmisnaza@gmail.com>
2019-03-26 20:53:26 -0500

    Push inicial

---
+++ src/main/java/com/jmisnaza/callcenter/CallCenterApplication.java
@@ -0,0 +1,13 @@
+package com.jmisnaza.callcenter;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CallCenterApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CallCenterApplication.class, args);
+	}
+
+}
